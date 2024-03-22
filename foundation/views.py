80d67a6be1950d259608581ff627336be580fa2f
godeytech <godfredtenkorang9@gmail.com>
2024-03-22 08:26:15 +0000

    changes

--- foundation/views.py
+++ foundation/views.py
@@ -4,20 +4,64 @@ from django.contrib import messages
 
 # Create your views here.
 def index(request):
-    return render(request, 'foundation/index.html')
+    main_category = request.GET.get('main_category')
+
+    if main_category == None:
+        projects = Project.objects.order_by('image')
+    else:
+        projects = Project.objects.filter(category__name=main_category)
+    main_categories = ProjectCategory.objects.all()
+    
+    context = {
+        'projects': projects,
+        'main_categories': main_categories,
+        'main_category': main_category
+    }
+    return render(request, 'foundation/index.html', context)
 
 def about(request):
-    return render(request, 'foundation/about.html')
+    main_category = request.GET.get('main_category')
+
+    if main_category == None:
+        projects = Project.objects.order_by('image')
+    else:
+        projects = Project.objects.filter(category__name=main_category)
+    main_categories = ProjectCategory.objects.all()
+
+    context = {
+        'projects': projects,
+        'main_categories': main_categories,
+        'main_category': main_category,
+        'title': 'About Us'
+    }
+    return render(request, 'foundation/about.html', context)
 
 def sponsorship(request):
+    main_category = request.GET.get('main_category')
+
+    if main_category == None:
+        projects = Project.objects.order_by('image')
+    else:
+        projects = Project.objects.filter(category__name=main_category)
+    main_categories = ProjectCategory.objects.all()
     sponsorships = Sponsorship.objects.all()
     context = {
+        'projects': projects,
+        'main_categories': main_categories,
+        'main_category': main_category,
         'sponsorships': sponsorships,
         'title': 'Sponsorship'
     }
     return render(request, 'foundation/sponsorship.html', context)
 
 def contact(request):
+    main_category = request.GET.get('main_category')
+
+    if main_category == None:
+        projects = Project.objects.order_by('image')
+    else:
+        projects = Project.objects.filter(category__name=main_category)
+    main_categories = ProjectCategory.objects.all()
     if request.method == 'POST':
         name = request.POST['name']
         email = request.POST['email']
@@ -27,11 +71,21 @@ def contact(request):
         contacts.save()
         return redirect('contact')
     context = {
+        'projects': projects,
+        'main_categories': main_categories,
+        'main_category': main_category,
         'title': "Contact Us"
     }
-    return render(request, 'foundation/contact.html')
+    return render(request, 'foundation/contact.html', context)
 
 def getinvolve(request):
+    main_category = request.GET.get('main_category')
+
+    if main_category == None:
+        projects = Project.objects.order_by('image')
+    else:
+        projects = Project.objects.filter(category__name=main_category)
+    main_categories = ProjectCategory.objects.all()
     if request.method == 'POST':
         first_name = request.POST['first_name']
         last_name = request.POST['last_name']
@@ -57,33 +111,76 @@ def getinvolve(request):
         getinvolve = VolunteerApplication(first_name=first_name, last_name=last_name, email=email, phone=phone, address=address, city=city, states=states, zipcode=zipcode, volunteer_position=volunteer_position, volunteer_experience=volunteer_experience, volunteer_skills=volunteer_skills, training_and_decrees_received=training_and_decrees_received, reason_for_volunteer=reason_for_volunteer, references=references, datetime_available=datetime_available, emergency_contact=emergency_contact, criminal_conviction=criminal_conviction, verified=verified, signature=signature, date=date, message=message)
         getinvolve.save()
         return redirect('getinvolve')
-    return render(request, 'foundation/getinvolve.html')
+    context = {
+        'projects': projects,
+        'main_categories': main_categories,
+        'main_category': main_category,
+        'title': "Get Involve"
+    }
+    return render(request, 'foundation/getinvolve.html', context)
 
 def watch(request):
+    main_category = request.GET.get('main_category')
+
+    if main_category == None:
+        projects = Project.objects.order_by('image')
+    else:
+        projects = Project.objects.filter(category__name=main_category)
+    main_categories = ProjectCategory.objects.all()
     videos = YouTube.objects.all()
     context = {
+        'projects': projects,
+        'main_categories': main_categories,
+        'main_category': main_category,
         'videos': videos,
         'title': 'videos'
     }
     return render(request, 'foundation/watch.html', context)
 
 def news(request):
+    main_category = request.GET.get('main_category')
+
+    if main_category == None:
+        projects = Project.objects.order_by('image')
+    else:
+        projects = Project.objects.filter(category__name=main_category)
+    main_categories = ProjectCategory.objects.all()
     blogs = News.objects.all()
     context = {
+        'projects': projects,
+        'main_categories': main_categories,
+        'main_category': main_category,
         'blogs': blogs,
         'title': 'news',
     }
     return render(request, 'foundation/news.html', context)
 
 def detail(request, news_slug):
+    main_category = request.GET.get('main_category')
+
+    if main_category == None:
+        projects = Project.objects.order_by('image')
+    else:
+        projects = Project.objects.filter(category__name=main_category)
+    main_categories = ProjectCategory.objects.all()
     blog = News.objects.get(slug=news_slug)
     context = {
+        'projects': projects,
+        'main_categories': main_categories,
+        'main_category': main_category,
         'blog': blog,
         'title': 'news details'
     }
     return render(request, 'foundation/detailPage.html', context)
 
 def photos(request):
+    main_category = request.GET.get('main_category')
+
+    if main_category == None:
+        projects = Project.objects.order_by('image')
+    else:
+        projects = Project.objects.filter(category__name=main_category)
+    main_categories = ProjectCategory.objects.all()
     category = request.GET.get('category')
     
     if category == None:
@@ -92,6 +189,9 @@ def photos(request):
         galleries = Gallery.objects.filter(category__title=category)
     categories = GalleryCategory.objects.all()
     context = {
+        'projects': projects,
+        'main_categories': main_categories,
+        'main_category': main_category,
         'categories': categories,
         'galleries': galleries,
         'title': 'Gallery'
@@ -99,6 +199,13 @@ def photos(request):
     return render(request, 'foundation/photos.html', context)
 
 def galleryDetail(request):
+    main_category = request.GET.get('main_category')
+
+    if main_category == None:
+        projects = Project.objects.order_by('image')
+    else:
+        projects = Project.objects.filter(category__name=main_category)
+    main_categories = ProjectCategory.objects.all()
     category = request.GET.get('category')
     gallery = Gallery.objects.filter(category__title=category).first()
     if category == None:
@@ -106,6 +213,9 @@ def galleryDetail(request):
     else:
         galleries = Gallery.objects.filter(category__title=category)
     context = {
+        'projects': projects,
+        'main_categories': main_categories,
+        'main_category': main_category,
         'category': category,
         'gallery': gallery,
         'galleries': galleries,
@@ -113,10 +223,38 @@ def galleryDetail(request):
     }
     return render(request, 'foundation/galleryDetail.html', context)
 
-def usaproject(request):
-    return render(request, 'foundation/usaProject.html')
+def project(request):
+    main_category = request.GET.get('main_category')
+    project = Project.objects.filter(main_category__name=main_category).first()
 
-def ghanaproject(request):
-    return render(request, 'foundation/ghanaProject.html')
+    if main_category == None:
+        projects = Project.objects.order_by('-date_added')
+    else:
+        projects = Project.objects.filter(category__name=main_category)
+    main_categories = ProjectCategory.objects.all()
+
+    context = {
+        'project': project,
+        'projects': projects,
+        'main_category': main_category,
+        'main_categories': main_categories,
+        'title': 'Projects'
+    }
+    return render(request, 'foundation/project.html', context)
+
 def donate(request):
-    return render(request, 'foundation/donate.html')
+    main_category = request.GET.get('main_category')
+
+    if main_category == None:
+        projects = Project.objects.order_by('-date_added')
+    else:
+        projects = Project.objects.filter(category__name=main_category)
+    main_categories = ProjectCategory.objects.all()
+    
+    context = {
+        'projects': projects,
+        'main_categories': main_categories,
+        'main_category': main_category,
+        'title': 'Donate'
+    }
+    return render(request, 'foundation/donate.html', context)
